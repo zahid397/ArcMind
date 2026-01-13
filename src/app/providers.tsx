5a14dbcf18b586febdea4a248365be19c8fc5f03
zahid397 <zh05698@gmail.com>
2026-01-14 05:17:24 +0600

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,8 +6,8 @@ import { Toaster } from 'sonner';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <Toaster richColors position="top-right" />
       {children}
+      <Toaster position="top-center" />
     </ThemeProvider>
   );
 }
