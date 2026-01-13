c94c48219156fb63b0a6c03098e595946a811772
zahid397 <zh05698@gmail.com>
2026-01-13 22:33:18 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   experimental: {
-    serverActions: true,
-  },
-  env: {
-    GROQ_API_KEY: process.env.GROQ_API_KEY,
+    appDir: true,
   },
 };
 
