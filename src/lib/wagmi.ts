24f3e95ecff7be5459b154caa3620bc299374c58
zahid397 <zh05698@gmail.com>
2026-01-14 06:27:16 +0600

    Update wagmi.ts

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,9 +1,13 @@
-import { createConfig, http } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
+import { http, createConfig } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { metaMask } from 'wagmi/connectors';
 
 export const config = createConfig({
-  chains: [mainnet],
+  chains: [sepolia],
+  connectors: [
+    metaMask()
+  ],
   transports: {
-    [mainnet.id]: http()
+    [sepolia.id]: http()
   }
 });
